3f6bd6b6dc8e109852721564c2d32f455f31d69e
Boris Lazebny <blazebny@zorallabs.com>
2019-06-06 18:38:05 +0300

    add actuator

--- sb2/src/main/java/ua/in/lbn/sb2/Application.java
+++ sb2/src/main/java/ua/in/lbn/sb2/Application.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    @SuppressWarnings("squid:S106")
     public static void main(String[] args) {
+        System.out.println("Swagger2: http://localhost/swagger-ui.html");
+        System.out.println("Actuator: http://localhost/actuator");
+
         SpringApplication.run(Application.class, args);
     }
 }
