0cd45cedc0aab070e158f972be6b548c886de2c2
Boris Lazebny <blazebny@zorallabs.com>
2019-06-06 19:39:55 +0300

    add integration test

---
+++ sb2/src/test/java/ua/in/lbn/sb2/rest/BuildPropertiesRestIT.java
@@ -0,0 +1,40 @@
+package ua.in.lbn.sb2.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BuildPropertiesRestIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getBuildProperties() throws Exception {
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/build")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.group", is("ua.in.lbn")))
+                .andReturn();
+
+        String s = result.getResponse().getContentAsString();
+
+        System.out.println(s);
+
+        assertNotNull(s);
+    }
+}
